9aef33e42603bc398a14ae07c4ac9f91cf3e5ae1
andrew <anemnox@gmail.com>
2023-08-19 17:50:06 -0700

    Added action states and improved managing animatioins

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const WIDTH = 640;
-const HEIGHT = 480;
+const WIDTH = 320;
+const HEIGHT = 240;
 
 const DEBUG_MODE = true;
 
@@ -16,11 +16,11 @@ const WALLS = [
 ]
 
 const FLOOR = {
-	x: WIDTH / 2, y: HEIGHT - 20, h: 20, w: WIDTH
+	x: WIDTH / 2, y: HEIGHT - 10, h: 20, w: WIDTH
 }
 
 const BASE_CD = {
-	dash: 100,
+	roll: 100,
 	dJump: 100,
 }
 
@@ -30,15 +30,28 @@ const BASE_CD = {
 const ANIMATIONS = {
 	run: { row: 0, frames: 8 },
 	jump: { row: 1, frames: 6, frameDelay: 8 },
-	roll: { row: 2, frames: 5, frameDelay: 14 },
+	fall: { row: 1, col: 4, frames: 1},
+	roll: { row: 2, frames: 5, frameDelay: 6 },
 	turn: { row: 3, frames: 7 },
 	stand: { row: 3, frames: 1 },
 	attack: { row: 10, frames: 10 },
 	die: { row: 14, frames: 5 }
 }
 
+const ACTIONS = {
+	NONE: "NONE",
+	IDLE: "IDLE",
+	ATTACK: "ATTACK",
+	ROLL: "ROLL"
+}
 
-let player = constructEntity();
+const ACTION_DATA = {
+	ATTACK: {cancelTime: 10, cancelable: true},
+	ROLL: {cancelable: false}
+}
+
+
+let player;
 
 let entityMap = {};
 let entityGroup;
@@ -78,124 +91,96 @@ function boundarySetUp() {
 function worldSetUp() {
 	allSprites.pixelPerfect = true;
 
-	world.gravity.y = 30;
+	world.gravity.y = 30 * -UP.y;
 
 	entityGroup = new Group();
 }
 
 function playerSetUp() {
-	player.sprite = new Sprite();
-	player.sprite.h = 32;
-	player.sprite.w = 32;
-
-	player.sprite.rotationLock = true;
-	player.sprite.friction = 0;
-	player.sprite.layer = 10; // player is drawn in front of enemy
-
-	//
-	//	Initialize Animations
-	//
-	player.sprite.anis.offset.x = 2;
-	player.sprite.anis.offset.y = -5;
-	player.sprite.anis.frameDelay = 4;
-
-	// Adding Animations based on ANIMATIONS constant
-	player.sprite.addAnis(playerImg, ANIMATIONS);
-
-	// Set the player Collision box. Use sprite.debug = true to see the
-	// Collision area
-	player.sprite.h = 20;
-	player.sprite.w = 16;
-	player.sprite.debug = DEBUG_MODE;
-
-	// Scale your player
-	player.sprite.scale = 4;
-
-	player.sprite.changeAni('stand');
-	entityMap[player.sprite] = player;
-	entityGroup.push(player.sprite);
-
-	// Setup attackhitbox
-	setupAttackHitbox(player);
+	player = constructEntity();
 }
 
 function constructEntity() {
-	return {
+	let entity = {
 		health: 100,
 		power: 10,
-		speed: 5,
-		jump: 12,
+		speed: 3,
+		jump: 10,
 		direction: 1,
+		action: ACTIONS.NONE,
+		actionTime: 0,
 		sprite: null,
 		attackHitbox: null,
 		cooldowns: {
-			dash: 0,
+			roll: 0,
 			dJump: 0
 		}
 	}
-}
-
-function spawnEnemy() {
-	let enemy = constructEntity();
-	enemy.sprite = new Sprite();
-	enemy.sprite.h = 32;
-	enemy.sprite.w = 32;
-	enemy.sprite.rotationLock = true;
-	enemy.sprite.friction = 0;
-	enemy.sprite.layer = 1;				// enemies are drawn behind player
+	entity.sprite = new Sprite();
+	entity.sprite.h = 32;
+	entity.sprite.w = 32;
+	entity.sprite.rotationLock = true;
+	entity.sprite.friction = 0;
+	entity.sprite.layer = 1;				// enemies are drawn behind player
 
 	//
 	//	Initialize Animations
 	//
-	enemy.sprite.anis.offset.x = 2;
-	enemy.sprite.anis.offset.y = -5;
-	enemy.sprite.anis.frameDelay = 4;
+	entity.sprite.anis.offset.x = 2;
+	entity.sprite.anis.offset.y = -5;
+	entity.sprite.anis.frameDelay = 4;
 
-	enemy.sprite.addAnis(playerImg, ANIMATIONS);
+	entity.sprite.addAnis(playerImg, ANIMATIONS);
 
 	// Set the player Collision box. Use sprite.debug = true to see the
 	// Collision area
-	enemy.sprite.h = 20;
-	enemy.sprite.w = 16;
-	enemy.sprite.debug = DEBUG_MODE;
+	entity.sprite.h = 20;
+	entity.sprite.w = 16;
+	entity.sprite.debug = DEBUG_MODE;
 
-	// Scale your enemy
-	enemy.sprite.scale = 4;
+	// Scale your entity
+	entity.sprite.scale = 2;
 
-	enemy.sprite.changeAni('stand');
+	entity.sprite.changeAni('stand');
 
 	// Make sure that entities overlap each other
 	let entities = Object.values(entityMap);
 	for (let i = 0; i < entities.length; i++) {
 		let e = entities[i];
-		enemy.sprite.overlaps(e.sprite);
+		entity.sprite.overlaps(e.sprite);
 	}
 
-	entityMap[enemy.sprite] = enemy;
-	entityGroup.add(enemy.sprite);
+	entityMap[entity.sprite] = entity;
+	entityGroup.add(entity.sprite);
+
+	setupAttackHitbox(entity);
+	return entity;
+}
+
+function spawnEnemy() {
+	let enemy = constructEntity();
+	
 	return enemy;
 }
 
 function setupAttackHitbox(entity) {
 	entity.attackHitbox = new Sprite();
-	entity.attackHitbox.w = 40;
-	entity.attackHitbox.h = 20;
-	entity.attackHitbox.x = (entity.sprite.x + (50 * entity.direction));
-	entity.attackHitbox.y = entity.sprite.y;
+	entity.attackHitbox.w = 20;
+	entity.attackHitbox.h = 10;
 	entity.attackHitbox.mass = 0;
 	entity.attackHitbox.visible = DEBUG_MODE;
 	entity.attackHitbox.debug = DEBUG_MODE;
 
 	entity.joint = new GlueJoint(entity.sprite, entity.attackHitbox);
 	entity.joint.visible = DEBUG_MODE;
+	updateHitbox(entity);
 }
 
 function updateHitbox(entity) {
 	entity.joint.remove();
-	entity.attackHitbox.x = (entity.sprite.x + (50 * entity.direction));
+	entity.attackHitbox.x = (entity.sprite.x + (25 * entity.direction));
 	entity.joint = new GlueJoint(entity.sprite, entity.attackHitbox);
 }
-
 
 function setup() {
 	let canvas = new Canvas(WIDTH, HEIGHT, 'pixelated');
@@ -218,13 +203,39 @@ function updateAllEntities(dt) {
 }
 
 function updateEntity(entity, dt) {
+	if (entity != player) soulAI(entity, dt);
+
+	entity.actionTime += dt;
 	Object.keys(entity.cooldowns).forEach((key) => {
 		entity.cooldowns[key] -= dt;
 		if (entity.cooldowns[key] < 0) {
 			entity.cooldowns[key] = 0;
 		}
 	});
-	if (entity != player) soulAI(entity, dt);
+	handleEntityAction(entity, dt);
+	handleEntityAnimation(entity, dt);
+}
+
+function handleEntityAnimation(entity, dt) {
+	if (entity.action == ACTIONS.NONE) {
+		if (isTouchingFloor(entity)) {
+			if (entity.sprite.velocity.x != 0) {
+				entity.sprite.changeAni("run");
+			} else {
+				entity.sprite.changeAni("stand");
+			}
+		} else {
+			entity.sprite.changeAni("fall");
+		}
+	}
+}
+
+function handleEntityAction(entity, dt) {
+	if (entity.action == ACTIONS.ROLL) {
+		entity.sprite.velocity.x = normalizeScalar(
+				entity.sprite.velocity.x
+			) * entity.speed * 1.5;
+	}
 }
 
 function soulAI(soul, dt) {
@@ -237,32 +248,44 @@ function soulAI(soul, dt) {
 //
 
 function attack(entity) {
-	updateHitbox(entity);
-	entity.sprite.changeAni(["attack", 'stand']);
-	
-	for(let i = 0; i < entityGroup.length; i++) {
-		let e = entityGroup[i];
-		if (entity.attackHitbox.overlapping(e)) {
-			damage(entityMap[e], entity.power);
+	if(canDoAction(entity, ACTIONS.ATTACK)) {
+		updateHitbox(entity);
+		entity.action = ACTIONS.ATTACK;
+		entity.actionTime = 0;
+
+		entity.sprite.changeAni("attack").then(() => resetAction(entity));
+
+
+		for(let i = 0; i < entityGroup.length; i++) {
+			let e = entityGroup[i];
+			if (entity.attackHitbox.overlapping(e)) {
+				damage(entityMap[e], entity.power);
+			}
 		}
 	}
 }
 
-function damage(entity, amount) {
-	entity.health -= amount;
-	console.log(entity.health);
+function roll(entity) {
+	if (entity.sprite.velocity.x &&
+		!entity.cooldowns.roll) {
+		entity.action = ACTIONS.ROLL;
+		entity.cooldowns.roll = BASE_CD.roll;
+		entity.actionTime = 0;
+
+		entity.sprite.changeAni("roll").then(() => resetAction(entity));
+	}
 }
 
+function resetAction(entity) {
+	entity.action = ACTIONS.NONE;
+}
+
+
 function move(entity, value) {
-	if(entity.sprite.ani.name == 'stand' || entity.sprite.ani.name == 'run') {
-		if (value > 0 || value < 0) {
-			entity.sprite.changeAni("run");
-			if(entity.direction != value) {
-				entity.direction = value;
-				entity.sprite.mirror.x = (value < 0);
-			}
-		} else {
-			entity.sprite.changeAni("stand");
+	if(entity.action == ACTIONS.NONE) {
+		if ((value > 0 || value < 0) && entity.direction != value) {
+			entity.direction = value;
+			entity.sprite.mirror.x = (value < 0);
 		}
 	}
 	entity.sprite.velocity.x = value * entity.speed;
@@ -270,26 +293,29 @@ function move(entity, value) {
 
 function jump(entity) {
 	if (isTouchingFloor(entity)) {
-		entity.sprite.changeAni(['jump', 'stand']);
-		entity.sprite.velocity.y = -entity.jump;
+		entity.sprite.velocity.y = entity.jump * UP.y;
 	} else if (!entity.cooldowns.dJump) {
-		entity.sprite.changeAni(['jump', 'stand']);
-		entity.sprite.velocity.y = -entity.jump;
+		entity.sprite.velocity.y = entity.jump * UP.y;
 		entity.cooldowns.dJump = BASE_CD.dJump;
-	}
-}
-
-function dash(entity) {
-	if (entity.sprite.velocity.x &&
-		!entity.cooldowns.dash) {
-		entity.sprite.velocity.x *= 20;
-		entity.cooldowns.dash = BASE_CD.dash;
 	}
 }
 
 function die(entity) {
 	entity.sprite.changeAni("die");
 }
+
+
+function damage(entity, amount) {
+	entity.health -= amount;
+}
+
+
+function canDoAction(entity, action) {
+	if (entity.action == ACTIONS.NONE) return true;
+
+	return false;
+}
+
 
 
 function inputHandle() {
@@ -299,10 +325,17 @@ function inputHandle() {
 	else if (kb.pressing('right')) move(player, 1);
 	else move(player, 0);
 
-	if (kb.presses('j')) dash(player);
+	if (kb.presses('j')) roll(player);
 	if (kb.presses('k')) attack(player);
 
 	if (kb.presses("g")) console.log(player.sprite);
+}
+
+function draw() {
+	background("beige");
+	inputHandle();
+
+	updateAllEntities(1);
 }
 
 function isTouchingFloor(entity) {
@@ -331,12 +364,7 @@ function vectorAngleComp(v1, v2) {
 	return v1.x * v2.x + v1.y * v2.y;
 }
 
-
-
-function draw() {
-	background("beige");
-
-	updateAllEntities(1);
-
-	inputHandle();
+function normalizeScalar(number) {
+	if (!number) return 0;
+	return Math.round(number / Math.abs(number));
 }
